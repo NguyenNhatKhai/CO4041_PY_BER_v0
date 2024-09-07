96e3b91c83ad75a201abf6e951cb4a41622aa670
NguyenNhatKhai <khai.nguyen352528@hcmut.edu.vn>
2024-09-07 13:59:33 +0700

    second commit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ MESSAGE = 239 * SYMBOL
 CAPABILITY = (CODEWORD - MESSAGE) // SYMBOL // 2
 
 ERROR_RATE = 0.001
+print(f"Prev FEC bits error rate:\t{ERROR_RATE :.16f}")
 
 symbol_error_indices = SYMBOL + 1
 symbol_error_rate = [0] * symbol_error_indices
@@ -26,13 +27,10 @@ print(f"Prev FEC message error rate:\t{prev_fec_message_error_rate :.16f}")
 post_fec_message_error_rate = sum(message_error_rate[i] if i > CAPABILITY else 0 for i in range(message_error_indices))
 print(f"Post FEC message error rate:\t{post_fec_message_error_rate :.16f}")
 
-post_fec_symbols_error_ratio = sum((i if i > CAPABILITY else 0) * message_error_rate[i] for i in range(message_error_indices))
-post_fec_symbols_error_rate = post_fec_message_error_rate * post_fec_symbols_error_ratio
-print(f"Post FEC symbols error ratio:\t{post_fec_symbols_error_ratio :.16f}")
+post_fec_symbols_error_rate = sum((i if i > CAPABILITY else 0) * message_error_rate[i] for i in range(message_error_indices)) / (MESSAGE // SYMBOL)
 print(f"Post FEC symbols error rate:\t{post_fec_symbols_error_rate :.16f}")
 
-post_fec_bits_error_ratio = sum((i if i > 0 else 0) * symbol_error_rate[i] / prev_fec_symbols_error_rate for i in range(symbol_error_indices))
-post_fec_bits_error_rate = post_fec_message_error_rate * post_fec_symbols_error_ratio * post_fec_bits_error_ratio
-print(f"Post FEC bits error ratio:\t{post_fec_bits_error_ratio :.16f}")
+post_fec_bits_error_rate = post_fec_symbols_error_rate * sum(i * symbol_error_rate[i] / prev_fec_symbols_error_rate for i in range(symbol_error_indices)) / SYMBOL
 print(f"Post FEC bits error rate:\t{post_fec_bits_error_rate :.16f}")
+
 
